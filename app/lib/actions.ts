aeb08148759b8a9dca38964bb9e1ff42bc1f2534
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-06-07 19:10:59 +0500

    Add FormSchema and updateInvoice function for invoice management

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,15 @@ import { revalidatePath } from 'next/cache'
 import postgres from 'postgres'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
 const CreateInvoice = z.object({
   customerId: z.string(),
   amount: z.coerce.number(),
@@ -21,4 +30,24 @@ export async function createInvoice(formData: FormData) {
              VALUES (${customerId}, ${amount * 100}, ${status}, CURRENT_DATE)`
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
+}
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
